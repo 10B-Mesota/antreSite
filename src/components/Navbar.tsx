80b9b912a10c869acee57ee8e50176a0dd19c3d2
scipex <scipex.dev@gmail.com>
2022-03-21 22:21:42 +0200

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,20 +6,26 @@ interface NavbarProps {}
 export const Navbar: React.FC<NavbarProps> = ({}) => {
   return (
     <div className="flex bg-negru p-3 text-white border-b-2 border-accent font-medium text-xl">
-      Pheonix
+      <div className="ml-5">Pheonix</div>
       <div className="text-portocaliu ml-2">Games</div>
-      <div className="ml-auto flex">
+      <div className="ml-auto flex mr-5">
         <Link href={"/"}>
-          <div className="mr-3 hover:underline">Acasa</div>
+          <div className="mr-3 hover:underline hover:text-portocaliu">
+            Acasa
+          </div>
         </Link>
         <Link href={"/about"}>
-          <div className="mr-3 hover:underline">Despre Noi</div>
+          <div className="mr-3 hover:underline hover:text-portocaliu">
+            Despre Noi
+          </div>
         </Link>
         <Link href={"/contact"}>
-          <div className="mr-3 hover:underline">Contact</div>
+          <div className="mr-3 hover:underline hover:text-portocaliu">
+            Contact
+          </div>
         </Link>
         <Link href={"/pricing"}>
-          <div className="mr-3 hover:underline">Pret</div>
+          <div className="mr-3 hover:underline hover:text-portocaliu">Pret</div>
         </Link>
       </div>
     </div>
